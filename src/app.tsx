4af242ae22346ea5422073021202a38b61e4cacf
Alve Svarén <alve@hotmail.se>
2021-06-05 00:47:21 +0200

    Fixed double routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,58 +1,94 @@
 import React from 'react';
-import { BrowserRouter, NavLink, NavLinkProps, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, NavLinkProps, Route, Switch, useLocation, RouteComponentProps } from 'react-router-dom';
 import './app.sass';
-export interface AppProps {}
+export interface AppProps extends RouteComponentProps {}
 
-export interface AppState {}
+export interface AppState {
+    leftSize?: number;
+    resizing: boolean;
+}
 
 class App extends React.Component<AppProps, AppState> {
+    leftColRef: React.Ref<HTMLElement>;
     constructor(props: AppProps) {
         super(props);
-        this.state = {};
+        this.state = { resizing: false };
+        this.leftColRef = React.createRef();
+        this.handleDragging = this.handleDragging.bind(this);
+        this.HashPreservingNavLink = this.HashPreservingNavLink.bind(this);
+        this.HashNavLink = this.HashNavLink.bind(this);
+        
     }
-    HashNavLink(props: React.PropsWithoutRef<NavLinkProps>){
+
+    HashNavLink(props: React.PropsWithoutRef<NavLinkProps>) {
         return <NavLink isActive={() => window.location.hash === props.to} {...props} />;
     }
+
+    HashPreservingNavLink(props: React.PropsWithoutRef<NavLinkProps>) {
+        if (typeof props.to === 'object') {
+            throw new TypeError('Objects as to attribute is currently not supported');
+        }
+        console.log(window.location.hash);
+        return <NavLink {...props} to={{ pathname: props.to.toString(), hash: this.props.location.hash }} />;
+    }
+
+    handleDragging(e: React.MouseEvent<HTMLDivElement>) {
+        if (this.state.resizing) {
+            this.setState({ leftSize: e.clientX });
+            document.documentElement.style.cursor = 'ew-resize';
+        } else {
+            document.documentElement.style.cursor = 'unset';
+        }
+    }
+
+    componentDidUpdate() {
+    }
+
     render() {
         return (
-            <BrowserRouter>
-                <div id='app'>
-                    <aside id='left-col'>
-                        <nav>
-                            <ul className='tabbed'>
-                                <li>
-                                    <this.HashNavLink to='#file'>File</this.HashNavLink>
-                                </li>
-                                <li>
-                                    <this.HashNavLink to='#hierarchy'>Hierarchy</this.HashNavLink>
-                                </li>
-                                <li>
-                                    <this.HashNavLink to='#settings'>Settings</this.HashNavLink>
-                                </li>
-                            </ul>
-                        </nav>
-                        <Switch></Switch>
-                    </aside>
-                    <main id='right-col'>
-                        <nav>
-                            <ul className='tabbed'>
-                                <li className="selected">
-                                    <NavLink to='/bodies'>Bodies</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='/world'>World</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='/3d'>3d</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='/map'>Map</NavLink>
-                                </li>
-                            </ul>
-                        </nav>
-                    </main>
-                </div>
-            </BrowserRouter>
+            <div
+                id='app'
+                onMouseMove={this.handleDragging}
+                onMouseUp={() => {
+                    if (this.state.resizing) this.setState({ resizing: false });
+                }}>
+                <aside ref={this.leftColRef} style={{ width: this.state.leftSize }} id='left-col'>
+                    <nav>
+                        <ul className='tabbed'>
+                            <li>
+                                <this.HashNavLink to='#file'>File</this.HashNavLink>
+                            </li>
+                            <li>
+                                <this.HashNavLink to='#hierarchy'>Hierarchy</this.HashNavLink>
+                            </li>
+                            <li>
+                                <this.HashNavLink to='#settings'>Settings</this.HashNavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch></Switch>
+                </aside>
+                <div className='resize-vertical-divider' onMouseDown={() => this.setState({ resizing: true })} />
+                <main id='right-col'>
+                    <nav>
+                        <ul className='tabbed'>
+                            <li className='selected'>
+                                <this.HashPreservingNavLink to='/bodies'>Bodies</this.HashPreservingNavLink>
+                            </li>
+                            <li>
+                                <this.HashPreservingNavLink to='/world'>World</this.HashPreservingNavLink>
+                            </li>
+                            <li>
+                                <this.HashPreservingNavLink to='/3d'>3d</this.HashPreservingNavLink>
+                            </li>
+                            <li>
+                                <this.HashPreservingNavLink to='/map'>Map</this.HashPreservingNavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch></Switch>
+                </main>
+            </div>
         );
     }
 }
