53a611e083ae5a8e2bce98ab5491532f414730a5
Alve Svarén <alve@hotmail.se>
2021-06-06 01:01:11 +0200

    Upload styling

--- src/components/common/dropTarget/dropTarget.tsx
+++ src/components/common/dropTarget/dropTarget.tsx
@@ -8,18 +8,31 @@ export interface DropTargetProps {
 
 export interface DropTargetState {
     currentlyOver: boolean;
+    processing: boolean;
+    percentage: number;
 }
 
 class DropTarget extends React.Component<DropTargetProps, DropTargetState> {
+    areaRef: React.RefObject<HTMLDivElement>;
     constructor(props: DropTargetProps) {
         super(props);
-        this.state = { currentlyOver: false };
+        this.areaRef = React.createRef();
+        this.state = { currentlyOver: false, processing: false, percentage: 0 };
     }
 
     handleDrop(event: React.DragEvent<HTMLDivElement>) {
         event.preventDefault();
         console.log(event.dataTransfer.files);
-        this.setState({ currentlyOver: false });
+        this.setState({ currentlyOver: false, processing: true });
+        setTimeout(() => {
+            this.setPercentage(100);
+        }, 100);
+        setTimeout(() => {
+            this.setState({processing: false})
+            setTimeout(() => {
+                this.setState({percentage: 0});
+            }, 2000)
+        }, 2000)
     }
 
     handleDragOver(event: React.DragEvent<HTMLDivElement>) {
@@ -32,10 +45,24 @@ class DropTarget extends React.Component<DropTargetProps, DropTargetState> {
         this.setState({ currentlyOver: false });
     }
 
+    setPercentage(num: number) {
+        this.setState({percentage: num});
+    }
+
+    componentDidUpdate() {
+        this.areaRef.current?.style.setProperty("--percentage", this.state.percentage + "%");
+    }
+
     render() {
+        
         return (
             <div
-                className={'drop-area' + (this.state.currentlyOver ? ' over' : '')}
+                ref={this.areaRef}
+                className={
+                    'drop-area' +
+                    (this.state.currentlyOver ? ' over' : '') +
+                    (this.state.processing ? ' processing' : '')
+                }
                 onDragOver={this.handleDragOver.bind(this)}
                 onDrop={this.handleDrop.bind(this)}
                 onDragLeave={this.handleDragLeave.bind(this)}>
